265336313b3b3634d1103917cdb531a140a05613
Surajit Tunga <surajittunga2005@gmail.com>
2025-06-15 21:31:53 +0530

    Create one.js

---
+++ one.js
@@ -0,0 +1,38 @@
+/*  History of Javascript:
+
+It's original Name is Mocha, then renamed to LiveScript and finaly javascript.
+Created by Brendan Eich in 1995.
+
+It is interpreted Language, Executed line by line.
+It is a high level dynamic programming language used for creating interactive effects with in web browsers, enables interactivity.
+Responds to the user, Fetches and Sends Data into server.
+
+
+
+Applications:
+
+React: A library for building User Interfaces.
+Angular: A platfrom for building mobile and desktop web application
+Many more............
+
+Node.JS: Run the server
+....
+
+Mobile application:React Native,Ionic,NativeScripts.....
+
+BuildTools: Webpack, Parcel...
+
+Desktop: Electron, NW.js...
+
+3D Programming: ThreeJS
+
+
+
+Runtime Environment:
+
+For Chrome V8: V8 is a tool in chrome which runs JS
+For FireFox SpiderMonkey
+For Edge Chakra
+
+
+*/
